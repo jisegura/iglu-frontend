0c9b2fb1ac631ea8b1247012cb17efcdde499f73
jisegura <segurajuanignacio@outlook.com>
2018-08-16 11:21:06 -0300

    config diferent product status

--- src/app/iglu-producto-modal/iglu-producto-modal.component.ts
+++ src/app/iglu-producto-modal/iglu-producto-modal.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Producto } from '../producto.model';
 
+export class MODE {
+  public POST: string = "POST";
+  public PUT: string = "PUT";
+  public DELETE: string = "DELETE";
+}
 
 @Component({
   selector: 'app-iglu-producto-modal',
@@ -11,16 +16,45 @@ import { Producto } from '../producto.model';
 export class IgluProductoModalComponent implements OnInit {
 
   public hide: boolean = true;
+  private mode: MODE;
 
   public constructor(
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   public ngOnInit(): void{
+    this.mode = new MODE;
   }
 
   public getTitulo(): string{
-    return this.data.titulo;
+    if (this.mode.POST == this.data.titulo) {
+      return "Agregar";
+    } else if (this.mode.PUT == this.data.titulo) {
+      return "Editar";
+    } else {
+      return "Eliminar";
+    }
+  }
+
+  public showSelect(): boolean{
+    if (this.mode.POST == this.data.titulo) {
+      return false;
+    }
+    return true;
+  }
+
+  public showCheckBox(): boolean{
+    if (this.mode.PUT == this.data.titulo) {
+      return true;
+    }
+    return false;
+  }
+
+  public showInput(): boolean{
+    if (this.mode.DELETE == this.data.titulo) {
+      return false;
+    }
+    return true;
   }
 
 }
