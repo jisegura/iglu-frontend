f9a0c86ee4631e9232fb03ac7c383a8b36a1397b
jisegura <segurajuanignacio@outlook.com>
2018-08-17 23:24:27 -0300

    set all methods of categoria service

--- src/app/categoria-data.service.ts
+++ src/app/categoria-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Categoria } from './producto.model';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { HttpErrorHandler, HandleError } from './http-error-handler.service';
 
@@ -17,14 +17,80 @@ const httpOptions = {
 })
 export class CategoriaDataService {
 
-  private categoriaUrl: string = "categoria/";
+  public categorias: Observable<Categoria[]>;
+  private categoriaUrl: string;
   private handleError: HandleError;
+  private _categorias: BehaviorSubject<Categoria[]>;
+  private dataStore: {
+    categorias: Categoria[]
+  };
 
   constructor(
     private http: HttpClient,
     public httpErrorHandler: HttpErrorHandler,
   ) {
+    this.categoriaUrl = "categoria/";
+    this.dataStore = { categorias: [] };
+    this._categorias = <BehaviorSubject<Categoria[]>> new BehaviorSubject([]);
+    this.categorias = this._categorias.asObservable();
     this.handleError = httpErrorHandler.createHandleError('CategoriaService');
+  }
+
+  public loadAll(): void{
+    this.getCategoria().subscribe(categorias => {
+      this.dataStore.categorias = categorias;
+      this._categorias.next(Object.assign({}, this.dataStore).categorias);
+    });
+  }
+
+  public load(id: number | string): void{
+    this.getCategoriaById(id).subscribe(categoria => {
+      let notFound = true;
+
+      this.dataStore.categorias.forEach((item, index) => {
+        if (item.Id_categoria === categoria.Id_categoria) {
+          this.dataStore.categorias[index] = categoria;
+          notFound = false;
+        }
+      });
+
+      if (notFound) {
+        this.dataStore.categorias.push(categoria);
+      }
+
+      this._categorias.next(Object.assign({}, this.dataStore).categorias);
+    });
+  }
+
+  public create(categoria: Categoria): void{
+    this.addCategoria(categoria).subscribe(categoria => {
+      this.dataStore.categorias.push(categoria);
+      this._categorias.next(Object.assign({}, this.dataStore).categorias);
+    });
+  }
+
+  public update(categoria: Categoria): void{
+    this.updateCategoria(categoria).subscribe(categoria => {
+      this.dataStore.categorias.forEach((item, index) => {
+        if (item.Id_categoria === categoria.Id_categoria) {
+          this.dataStore.categorias[index] = categoria;
+        }
+      });
+
+      this._categorias.next(Object.assign({}, this.dataStore).categorias);
+    });
+  }
+
+  public remove(id: number): void{
+    this.deleteCategoria(id).subscribe(response => {
+      this.dataStore.categorias.forEach((item, index) => {
+        if (item.Id_categoria === id) {
+          this.dataStore.categorias.splice(index, 1);
+        }
+      });
+
+      this._categorias.next(Object.assign({}, this.dataStore).categorias);
+    });
   }
 
   public getCategoria(): Observable<Categoria[]>{
@@ -33,9 +99,29 @@ export class CategoriaDataService {
     );
   }
 
+  public getCategoriaById(id: number | string): Observable<Categoria>{
+    const url = `${this.categoriaUrl}${id}`;
+    return this.http.get<Categoria>(url).pipe(
+      catchError(this.handleError<Categoria>(`getCategoriaById id=${id}`))
+    );
+  }
+
   public addCategoria(categoria: Categoria): Observable<Categoria>{
     return this.http.post<Categoria>(this.categoriaUrl, categoria, httpOptions).pipe(
       catchError(this.handleError('addCategoria', categoria))
+    );
+  }
+
+  public updateCategoria(categoria: Categoria): Observable<Categoria>{
+    return this.http.put<Categoria>(this.categoriaUrl, categoria, httpOptions).pipe(
+      catchError(this.handleError('updateCategoria', categoria))
+    );
+  }
+
+  public deleteCategoria(id: number): Observable<{}>{
+    const url = `${this.categoriaUrl}${id}`;
+    return this.http.delete(url, httpOptions).pipe(
+      catchError(this.handleError('deleteCategoria'))
     );
   }
 
