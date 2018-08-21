7b1cf43920b0a85625c61cb2dcc9ebec51682854
jisegura <segurajuanignacio@outlook.com>
2018-08-20 21:48:40 -0300

    add hama ip

--- src/app/http-interceptors/server-interceptor.ts
+++ src/app/http-interceptors/server-interceptor.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class ServerInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const serverReq = req.clone({
-      url: `http://192.168.1.36:3000/${req.url}`
+      url: `http://25.71.37.25:3000/${req.url}`
     });
     return next.handle(serverReq);
   }
