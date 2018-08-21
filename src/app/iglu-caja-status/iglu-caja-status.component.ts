b1f57db446c3bfb1051fdf0b527579f7ddba6b90
jisegura <segurajuanignacio@outlook.com>
2018-08-21 13:41:57 -0300

    add caja status

--- src/app/iglu-caja-status/iglu-caja-status.component.ts
+++ src/app/iglu-caja-status/iglu-caja-status.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CajaDataService } from '../caja-data.service';
+import { Caja } from '../caja.model';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-iglu-caja-status',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IgluCajaStatusComponent implements OnInit {
 
-  constructor() { }
+  public cajaOpen: Observable<Caja>;
+
+  constructor(
+  	private cadaDataService: CajaDataService
+  ) { }
 
   ngOnInit() {
+    this.cajaOpen = this.cadaDataService.cajaOpen;
+    this.cadaDataService.loadCajaOpen();
+  }
+
+  public haveCajaOpen(): boolean{
+    return true;
   }
 
 }
